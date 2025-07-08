f6c7d8ef4dd5a7bd5dd8527a453c12a24bef95bc
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2025-07-08 13:26:49 +0200

    Create 20_valid_parentheses.py

---
+++ src/20_valid_parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        parentheses = {
+        ')' : '(', 
+        '}' : '{', 
+        ']' : '['
+        }
+
+        for character in s:
+            if character in parentheses:
+                if stack and stack[-1] == parentheses[character]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(character)
+
+        return len(stack) == 0
+        
