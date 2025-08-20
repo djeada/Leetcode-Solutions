0eb531dc4fe1591757d031912d59bfb9dcee2210
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2025-08-20 22:18:40 +0200

    Update 347_top_k_frequent_elements.py

--- src/347_top_k_frequent_elements.py
+++ src/347_top_k_frequent_elements.py
@@ -1,1 +1,19 @@
 
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        counter = Counter(nums)
+        max_f = max(counter.values())
+        buckets = [[] for i in range(max_f + 1)]
+
+        for num, freq in counter.items():
+            buckets[freq].append(num)
+
+        result = list()
+        for bucket in reversed(buckets):
+            for num in bucket:
+                result.append(num)
+                if len(result) == k:
+                    return result
+
+        return list()
